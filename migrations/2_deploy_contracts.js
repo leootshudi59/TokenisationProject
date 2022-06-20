9334065e13cfcf7c4f277b9bd2396d7432fa123a
Léo OTSHUDI <leootshudi@gmail.com>
2022-06-20 16:00:27 +0200

    feat: crowdsale init + crowdsale unit test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 var BCPL = artifacts.require("../contracts/BCPL.sol");
+var BCPLSale = artifacts.require("../contracts/BCPLSale.sol");
+
+require('dotenv').config({path:'../.env'});
 
 module.exports = async function(deployer) {
-    await deployer.deploy(BCPL, 100000);
+    let addresses = await web3.eth.getAccounts();
+    await deployer.deploy(BCPL, process.env.INITIAL_TOKENS_SUPPLY);
+    await deployer.deploy(BCPLSale, 1, addresses[0], BCPL.address);
+    let tokenInstance = await BCPL.deployed();
+    await tokenInstance.transfer(BCPLSale.address, process.env.INITIAL_TOKENS_SUPPLY);
 }
--- test/BCPL.test.js
+++ test/BCPL.test.js
@@ -1,38 +1,49 @@
 const BCPL = artifacts.require("../contracts/BCPL.sol");
 
-var chai = require("chai");
+require('dotenv').config({path:'../.env'});
+
+const chai = require("./chaiSetup.js");
 const BN = web3.utils.BN;
-const chaiBN = require("chai-bn")(BN);
-chai.use(chaiBN);
-
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
 const expect = chai.expect;
 
-contract("BCPL", async (accounts) => {
-  const [contractDeployerAccount, recipientAccount] = accounts;
+contract("BCPL test", async (accounts) => {
+  const [contractDeployerAccount, recipientAccount, anotherAccount] = accounts;
+
+  // Each test launches with a new token instance with the initial supply tokens
+  beforeEach(async () => {
+    this.bpcl = await BCPL.new(process.env.INITIAL_TOKENS_SUPPLY)
+  })
+
   it("all tokens should be in my account", async () => {
-    let tokenInstance = await BCPL.deployed();
+    let tokenInstance = this.bpcl;
     let totalSupply = await tokenInstance.totalSupply();
     expect(tokenInstance.balanceOf(contractDeployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
   });
 
-  it("... lets tokens to be sent between accounts", async () => {
-    const amountOfTokensToSend = 7;
-    let tokenInstance = await BCPL.deployed();
-    let totalSupply = await tokenInstance.totalSupply();
-    expect(tokenInstance.transfer(recipientAccount, amountOfTokensToSend)).to.eventually.be.fulfilled;
-    //console.log(totalSupply.sub(new BN(amountOfTokensToSend)));
-    expect(tokenInstance.balanceOf(contractDeployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(amountOfTokensToSend)));
-    expect(tokenInstance.balanceOf(recipientAccount)).to.eventually.be.a.bignumber.equal(new BN(amountOfTokensToSend));
-  });
-
-
   it("... does not make it possible to send more tokens than available in deployer balance", async () => {
-    let tokenInstance = await BCPL.deployed();
+    let tokenInstance = this.bpcl;
     let deployerBalance = await tokenInstance.balanceOf(contractDeployerAccount);
 
     expect(tokenInstance.transfer(recipientAccount, new BN(deployerBalance + 1))).to.eventually.be.rejected;
   });
+
+  it("tokens can sent between accounts", async () => {
+    const amountOfTokensToSend = 7;
+    let tokenInstance = this.bpcl;
+    let totalSupply = await tokenInstance.totalSupply();
+    expect(tokenInstance.balanceOf(contractDeployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    return expect(tokenInstance.transfer(recipientAccount, amountOfTokensToSend)).to.eventually.be.fulfilled;
+    //console.log(totalSupply.sub(new BN(amountOfTokensToSend)));
+    //expect(tokenInstance.balanceOf(contractDeployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(amountOfTokensToSend)));
+    //return expect(tokenInstance.balanceOf(recipientAccount)).to.eventually.be.a.bignumber.equal(new BN(amountOfTokensToSend));
+  });
+
+  it("sender and recipient account balances should be updated when tokens are sent to each other", async () => {
+    const amountOfTokensToSend = 7;
+    let tokenInstance = this.bpcl;
+    let totalSupply = await tokenInstance.totalSupply();
+    await tokenInstance.transfer(recipientAccount, amountOfTokensToSend)
+    expect(tokenInstance.balanceOf(contractDeployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(amountOfTokensToSend)));
+    return expect(tokenInstance.balanceOf(recipientAccount)).to.eventually.be.a.bignumber.equal(new BN(amountOfTokensToSend));
+  })
 });
